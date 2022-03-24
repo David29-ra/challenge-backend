88f94dac4979718bdd2f1f7e65f87a39ed6f4ba6
David29-ra <davidrivasa.12@gmail.com>
2022-03-23 23:57:00 -0500

    Defined functions for each quiestion of the challenge, also requests package are added to get data from pokeapi

--- solutions.py
+++ solutions.py
@@ -1,1 +1,36 @@
-# here is the solutions for the challenge
+import requests
+
+BASE_URL = "https://pokeapi.co/api/v2/"
+
+def names_with_special_characters() -> int:
+    """
+    Return how many pokemon names that contain 'at' and double 'a'.
+    latias: it should be included in the count because
+            has an 'at' and a double 'a'.
+    rattata: it should not be included in the count because
+              has two 'at' and triple 'a'.
+    """
+    pass
+
+
+def  breed_pokemons()-> int:
+    """
+    Return how many pokemon can breed with Raichu, two pokemons
+    can breed if they belong to the same egg group.
+    """
+    pass
+
+
+def get_max_and_min_weight() -> list:
+    """
+    Return the max and min weight of fighting type pokemon.
+    """
+    pass
+
+first_answer = names_with_special_characters()
+second_answer = breed_pokemons()
+third_answer = get_max_and_min_weight()
+
+print(f"There are {first_answer} pokemon names with 'at' and double 'a'.")
+print(f"There are {second_answer} pokemon that can breed with Raichu.")
+print(f"The max and min weight of fighting type pokemon are {third_answer}.")
