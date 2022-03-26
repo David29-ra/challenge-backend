0c67724071f725fd7bab7dfab5c6634a503df23a
David29-ra <davidrivasa.12@gmail.com>
2022-03-26 00:08:21 -0500

    Add RequestError class to handle error if there are some problem in requests

--- solutions.py
+++ solutions.py
@@ -2,12 +2,29 @@ import requests
 from time import time
 BASE_URL = "https://pokeapi.co/api/v2/"
 
-def api_response(url) -> dict :
+class RequestError(Exception):
+    """
+    Raise this error when the request fails.
+
+    Attributes:
+        status_code -- input salary which caused the error
+        message -- explanation of the error
+    """
+    def __init__(self, status_code, url, message = "Error in the request for url"):
+        self.status_code = status_code
+        self.message = message + ": {}".format(url)
+        super().__init__(self.message)
+
+
+def api_response(url) -> dict or str:
     """
     Return the response of the api in json format.
     """
     response = requests.get(url)
-    return response.json()
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise RequestError(response.status_code, url)
 
 
 def names_with_special_characters() -> int:
@@ -21,19 +38,14 @@ def names_with_special_characters() -> int:
     count = 0
     # requests
     url_one_pokemon = BASE_URL + "pokemon?limit=1"
-    # response = requests.get(BASE_URL + f"pokemon?limit=1")
-    # response_data = response.json()
     limit = api_response(url_one_pokemon)['count']
-
+    
     # requests
     url_all_pokemon = BASE_URL + f"pokemon?limit={limit}"
     pokemons = api_response(url_all_pokemon)['results']
-    # response = requests.get(BASE_URL + f"pokemon?limit={limit}")
-    # pokemons = response.json()["results"]
 
     for pokemon in pokemons:
         if pokemon["name"].count('a') == 2 and pokemon["name"].count('at') >= 1:
-            # print(pokemon['name'])
             count += 1
             
     return count
@@ -46,24 +58,20 @@ def breed_pokemons()-> int:
     """
     pokemon = "raichu"
     # requests
-    # response_specie = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{pokemon}')
-    # egg_groups = response_specie.json()["egg_groups"]
     url_pokemon_specie = BASE_URL + f"pokemon-species/{pokemon}"
     egg_groups = api_response(url_pokemon_specie)["egg_groups"]
 
     pokemons_can_breed = []
     for egg_group in egg_groups:
         # requests
-        # response_group = requests.get(egg_group["url"])
-        # pokemons_specie = response_group.json()["pokemon_species"]
         url_egg_group = egg_group["url"]
         pokemons_specie = api_response(url_egg_group)["pokemon_species"]
 
         for pokemon_specie in pokemons_specie:
             pokemons_can_breed.append(pokemon_specie["name"])
     
-    # print (pokemons_can_breed)
     pokemons_can_breed_uniq = set(pokemons_can_breed)
+
     return len(pokemons_can_breed_uniq)
 
 
@@ -79,18 +87,13 @@ def get_max_and_min_weight() -> list:
     """
     pokemon_type = "fighting"
     # requests
-    # response = requests.get(f"https://pokeapi.co/api/v2/type/{pokemon_type}")
-    # fighting_pokemons = response.json()['pokemon']
     url_pokemon_type = BASE_URL + f"type/{pokemon_type}"
     fighting_pokemons = api_response(url_pokemon_type)["pokemon"]
 
     weigths = []
-
     for pokemon in fighting_pokemons:
         if get_id(pokemon['pokemon']['url']) <= 151:
             # requests
-            # pokemon_response = requests.get(pokemon['pokemon']['url'])
-            # pokemon_data = pokemon_response.json()
             url_pokemon = pokemon['pokemon']['url']
             pokemon_data = api_response(url_pokemon)['weight']
 
@@ -101,24 +104,20 @@ def get_max_and_min_weight() -> list:
 
     return [max_weight, min_weight]
 
-start_time1 = time()
-first_answer = names_with_special_characters()
-elapsed_time1 = time() - start_time1
+start_time = time()
+try:
+    first_answer = names_with_special_characters()
+    second_answer = breed_pokemons()
+    third_answer = get_max_and_min_weight()
+except KeyboardInterrupt:
+    print("\nYou pressed Ctrl+C. Exiting...")
+except RequestError as error:
+    print(f"\n{error}")
+    print("\nTry again...")
+else:
+    print(f"There are {first_answer} pokemon names with 'at' and double 'a'.")
+    print(f"There are {second_answer} pokemon that can breed with Raichu.")
+    print(f"The max and min weight of fighting type pokemon are {third_answer}.")
+elapsed_time = time() - start_time
 
-start_time2 = time()
-second_answer = breed_pokemons()
-elapsed_time2 = time() - start_time2
-
-start_time3 = time()
-third_answer = get_max_and_min_weight()
-elapsed_time3 = time() - start_time3
-
-
-print(f"There are {first_answer} pokemon names with 'at' and double 'a'.")
-print("Elapsed time1: %.10f seconds" % elapsed_time1)
-
-print(f"There are {second_answer} pokemon that can breed with Raichu.")
-print("Elapsed time2: %.10f seconds" % elapsed_time2)
-
-print(f"The max and min weight of fighting type pokemon are {third_answer}.")
-print("Elapsed time3: %.10f seconds" % elapsed_time3)
+print("Elapsed time1: %.10f seconds" % elapsed_time)
